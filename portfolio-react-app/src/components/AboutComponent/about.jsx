b479e2dfef83ef0550b79fb67b8ffc8eee93e7ae
Mike <mxbalogun@gmail.com>
2025-04-02 21:47:36 -0400

    Created about component file structure

---
+++ portfolio-react-app/src/components/AboutComponent/about.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './about.module.css';
+
+const About = () => {
+    return (
+        <>
+            <section id={styles.aboutContainer}>
+        
+                <div>
+                    <h1>About me</h1>
+                </div>
+
+                <div>
+                    <img src="" alt="Image of me" />
+                </div>
+            </section>
+        </>
+    
+    )
+}
+
+export default About;
