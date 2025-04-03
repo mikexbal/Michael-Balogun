d6340180edb747c3d596175e53129d5ffbc9aac5
Mike <mxbalogun@gmail.com>
2025-04-02 21:47:57 -0400

    Created component folder structure

---
+++ portfolio-react-app/src/components/EducationComponent/education.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from './education.module.css';
+
+const Education = () => {
+    return (
+        <>
+        <section id={styles.educationContainer}>
+            <h1>Education Section</h1>
+
+        </section>
+
+        </>
+    )
+}
+
+export default Education;
