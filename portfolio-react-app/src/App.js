dc9b018df63390408a5ec6c91fffeb969a6fb825
Mike <mxbalogun@gmail.com>
2025-04-02 21:46:56 -0400

    Added components

--- portfolio-react-app/src/App.js
+++ portfolio-react-app/src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import Header from './components/HeaderComponent/header';
+import Footer from './components/FooterComponent/footer';
+import About from './components/AboutComponent/about';
+import Education from './components/EducationComponent/education';
+import Experience from './components/ExperienceComponent/experience';
+import Projects from './components/ProjectsComponent/projects';
+
+import TechStack from './components/TechComponent/techstack';
+
+
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <About />
+      <Education />
+      <Experience />
+      <Projects />
+      <TechStack />
+      <Footer />
+    </>
     
   );
 }
