51e0c64bb28fe255641916adc9db64b1dd3c4d51
Mike <mxbalogun@gmail.com>
2025-04-02 21:49:07 -0400

    Created header component and styled, need to finish styling and fix navigation links... implement smooth scrolling or maybe react router

--- portfolio-react-app/src/components/HeaderComponent/header.jsx
+++ portfolio-react-app/src/components/HeaderComponent/header.jsx
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from 'reactstrap';
 
-const Header = () => {
-    return (
-        <h1>TESTINF</h1>
-    )
+import styles from './header.module.css';
+
+function Header(args) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+        <section id={styles.headerContainer}>
+        <Navbar {...args} expand="md">
+            <NavbarBrand href="/">Michael Balogun</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+            <Nav className={styles.navLinks} navbar>
+            <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                    About
+                </NavLink>
+                </NavItem>
+                <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                    Education
+                </NavLink>
+                </NavItem>
+                <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                    Work Experience
+                </NavLink>
+                </NavItem>
+                <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                    Projects
+                </NavLink>
+                </NavItem>
+                <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                    Tech Stack
+                </NavLink>
+                </NavItem>
+                <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                    Contact Me
+                </NavLink>
+                </NavItem>
+            </Nav>
+            </Collapse>
+        </Navbar>
+        </section>
+    </>
+  );
 }
 
 export default Header;
