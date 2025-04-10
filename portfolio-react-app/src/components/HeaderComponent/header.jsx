f5df25695f64eb5c8c5be84f9bd7820135390b05
Mike <mxbalogun@gmail.com>
2025-04-09 22:49:50 -0400

    Removed reactstrap and styled a new header and about section

--- portfolio-react-app/src/components/HeaderComponent/header.jsx
+++ portfolio-react-app/src/components/HeaderComponent/header.jsx
@@ -19,45 +19,29 @@ function Header(args) {
 
   return (
     <>
-        <section id={styles.headerContainer}>
-        <Navbar {...args} expand="md">
-            <NavbarBrand href="/">Michael Balogun</NavbarBrand>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
-            <Nav className={styles.navLinks} navbar>
-            <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                    About
-                </NavLink>
-                </NavItem>
-                <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                    Education
-                </NavLink>
-                </NavItem>
-                <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                    Work Experience
-                </NavLink>
-                </NavItem>
-                <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                    Projects
-                </NavLink>
-                </NavItem>
-                <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                    Tech Stack
-                </NavLink>
-                </NavItem>
-                <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                    Contact Me
-                </NavLink>
-                </NavItem>
-            </Nav>
-            </Collapse>
-        </Navbar>
+        <section id={styles.header}>
+            <div id={styles.headerContainer}>
+
+                <ul id={styles.headerList}>
+                    <li> <a href="" id={styles.headerLinks}>Home</a> </li>
+                    <li> <a href="" id={styles.headerLinks}>Experience</a> </li>
+                    <li> <a href="" id={styles.headerLinks}>Projects</a> </li>
+                    <li> <a href="" id={styles.headerLinks}>Tech Stack</a> </li>
+                </ul>
+            </div>
+           
+            <div id={styles.introduction}>
+                <h1>Michael Balogun.</h1>
+                <h3>Aspiring Full-Stack Engineer</h3>
+                <p>
+                    Undergraduate Software Engineer at <span style={{ color: "#A41E35" }}>Temple University</span>
+                    studying Information Science and Technology, passionate about bridging 
+                    the gap between people and technology through thoughtful, 
+                    user-centered solutions.
+                </p>
+                <button>Contact Me</button>
+                <button>My Work &#8594;</button>
+            </div>
         </section>
     </>
   );
