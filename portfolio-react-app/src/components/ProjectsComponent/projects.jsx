45f300b14db83fe3f30a72acc21c8ff2e948dfae
Mike <mxbalogun@gmail.com>
2025-04-10 22:14:08 -0400

    Structured project component

--- portfolio-react-app/src/components/ProjectsComponent/projects.jsx
+++ portfolio-react-app/src/components/ProjectsComponent/projects.jsx
@@ -5,35 +5,70 @@ import styles from './projects.module.css';
 const Projects = () => {
     return (
         <>
-        <section id={styles.projectsContainer}>
-            <h1 id ={styles.projectTitle}>Project 1</h1>
-            <ul id={styles.projectTechnologies}>
-                <li id={styles.technologyItem}>
-                    C#
-                </li>
-                
-                <li id={styles.technologyItem}>
-                    .NET Framework
-                </li>
-                <li id={styles.technologyItem}>
-                    Microsoft Access DB
-                </li>
+        <section id={styles.projectsSection}>
 
-            </ul>
+            <div id={styles.projectCard}>
+                <h1 id ={styles.projectTitle}>Car Rental Database</h1>
+
+                <p id={styles.projectDescription}>
+                    Developed a car rental application in C# using the .NET Framework 
+                    and Microsoft Access, implementing the MVC design pattern. 
+                    The system supports adding, renting, returning, and searching cars, 
+                    with user-friendly Windows Forms to streamline employee workflow. 
+                    SQL queries were used for seamless integration with the Access database, 
+                    ensuring efficient data management and transaction integrity.
+                </p>
+
+
+                <ul id={styles.projectTechnologies}>
+                    <li id={styles.technologyItem}>
+                        C#
+                    </li>
+                    
+                    <li id={styles.technologyItem}>
+                        .NET Framework
+                    </li>
+                    <li id={styles.technologyItem}>
+                        Microsoft Access DB
+                    </li>
+
+                </ul>
+
+            </div>
+
+            <div id={styles.projectCard}>
+                <h1 id ={styles.projectTitle}>Project 1</h1>
+
+                <p id={styles.projectDescription}>
+                    Developed a car rental application in C# using the .NET Framework 
+                    and Microsoft Access, implementing the MVC design pattern. 
+                    The system supports adding, renting, returning, and searching cars, 
+                    with user-friendly Windows Forms to streamline employee workflow. 
+                    SQL queries were used for seamless integration with the Access database, 
+                    ensuring efficient data management and transaction integrity.
+                </p>
+
+
+                <ul id={styles.projectTechnologies}>
+                    <li id={styles.technologyItem}>
+                        C#
+                    </li>
+                    
+                    <li id={styles.technologyItem}>
+                        .NET Framework
+                    </li>
+                    <li id={styles.technologyItem}>
+                        Microsoft Access DB
+                    </li>
+
+                </ul>
+
+            </div>
+
             
-            <p id={styles.projectDescription}>
-                Developed a car rental application in C# using the .NET Framework 
-                and Microsoft Access, implementing the MVC design pattern. 
-                The system supports adding, renting, returning, and searching cars, 
-                with user-friendly Windows Forms to streamline employee workflow. 
-                SQL queries were used for seamless integration with the Access database, 
-                ensuring efficient data management and transaction integrity.
-            </p>
             
             
             
-            <h1>Project 2</h1>
-            <h1>Project 3</h1>
 
         </section>
 
